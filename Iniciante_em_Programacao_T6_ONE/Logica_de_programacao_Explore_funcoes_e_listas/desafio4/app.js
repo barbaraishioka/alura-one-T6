ce063b50ce93ed5fabb1d1cecfdc88ec2f34773e
barbaraishioka <barbara.ishioka@gmail.com>
2024-01-26 16:46:22 -0300

    feat: add challenges

---
+++ Iniciante_em_Programacao_T6_ONE/Logica_de_programacao_Explore_funcoes_e_listas/desafio4/app.js
@@ -0,0 +1,43 @@
+// Desafios
+
+// Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+console.log(listaGenerica);
+
+// ___________________________________
+// ___________________________________
+
+// Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ["JavaScript", "C", "C++", "Kotlin", "Python"];
+console.log(linguagensDeProgramacao);
+
+// ___________________________________
+// ___________________________________
+
+// Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+// linguagensDeProgramacao.push("Java");
+// linguagensDeProgramacao.push("Ruby");
+// linguagensDeProgramacao.push("GoLang");
+linguagensDeProgramacao.push("Java", "Ruby", "GoLang");
+console.log(linguagensDeProgramacao);
+
+// ___________________________________
+// ___________________________________
+
+// Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let nomes = ["Ana", "Alex", "Paula"];
+console.log(nomes[0]);
+
+// ___________________________________
+// ___________________________________
+
+// Crie uma lista com 3 nomes2 e exiba no console apenas o segundo elemento.
+let nomes2 = ["Ana", "Alex", "Paula"];
+console.log(nomes2[1]);
+
+// ___________________________________
+// ___________________________________
+
+// Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+let nomes3 = ["Ana", "Alex", "Paula"];
+console.log(nomes3[nomes.length - 1]);
